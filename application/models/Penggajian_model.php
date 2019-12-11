70a509bcac7742e463adb61d7ba06044070f3149
Agung Asmara <dode.agung.asmara@gmail.com>
2019-12-11 15:01:18 +0800

    revisi 11 12 19

--- application/models/Penggajian_model.php
+++ application/models/Penggajian_model.php
@@ -205,7 +205,7 @@ class Penggajian_model extends CI_Model
                       ->group_by('rg.id')
                       ->get();
 
-        //echo ($this->db->last_query());              
+        echo ($this->db->last_query());              
         if($query->num_rows() > 0){
             return $query->result();
         }else{
